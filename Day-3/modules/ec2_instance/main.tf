7e6007c21418636e01f7449899d2f45853c70111
ankitakumari11 <81357184+ankitakumari11@users.noreply.github.com>
2025-06-14 21:52:47 +0530

    Update main.tf

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -1,9 +1,16 @@
-provider "aws" {
+provider "aws" {  
   region = "us-east-1"
 }
+# You can also create a file named providers.tf and write the provider like aws,oci,gcp etc in that.
 
 resource "aws_instance" "example" {
     ami = var.ami_value
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
 }
+
+
+# now to run the code , go to day-3 folder then run -
+# terraform init (this will initialise the terraform)
+# terraform plan (to see what changes are going to be made)
+# terraform apply (to apply the changes)
