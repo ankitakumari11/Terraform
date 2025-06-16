ca2311220305f107cd9c002b0d3b9289bdd667f8
ankitakumari11 <81357184+ankitakumari11@users.noreply.github.com>
2025-06-16 20:24:15 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
+    key            = "abhi/terraform.tfstate"  # this will store the terraform statefile inside abhi/ folder
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
