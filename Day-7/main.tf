b017d69d7b91391a8c533f676d25f483bf5c5f99
ankitakumari11 <81357184+ankitakumari11@users.noreply.github.com>
2025-06-21 19:18:32 +0530

    Update main.tf

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -3,20 +3,20 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:8200"
-  skip_child_token = true
+  address = "<>:8200"  # instance-ip:8200
+  skip_child_token = true # recommended by hashicorp
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
+      role_id = "<>" # you will take it from vault, use the commands mentioned in readme file 
       secret_id = "<>"
     }
   }
 }
 
-data "vault_kv_secret_v2" "example" {
+data "vault_kv_secret_v2" "example" { # data keyword used to read something from a platform
   mount = "secret" // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
